eb7ff90b37d25c0d3b127b63c55555607c903a71
lathamlm <130019215+lathamlm@users.noreply.github.com>
2023-09-23 14:43:54 -0400

    updated flask to connect to js/html

--- flask_app.py
+++ flask_app.py
@@ -47,10 +47,13 @@ def welcome():
         f"<br/>"
         f"<strong>This route contains a list of all the toxic releases on occuring on tribal lands:</strong><br/>"
         f"<p style='color:SlateBlue;'>/api/tribes</p>"
+        #f"<br/>"
+        #f"<strong>This route contains all toxic releases in 2021:</strong><br/>"
+        #f"<p style='color:Purple;'>/api/all</p>"
     )
 
 # LOCATION DATA - TOO LARGE FOR ALL, HAD TO SPLIT IT UP
-@app.route("/api/location")
+@app.route("/api/location", methods=["GET"])
 def location():
     session = Session(engine)
 
@@ -72,7 +75,10 @@ def location():
         location_dict["Industry Sector Code"] = INDUSTRY_SECTOR_CODE
         location_list.append(location_dict)        
 
-    return jsonify(location_list)
+    # REFERENCED https://stackoverflow.com/questions/25860304/how-do-i-set-response-headers-in-flask
+    response = jsonify(location_list)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 # VALUES (2ND HALF OF LOCATION DATA)
 @app.route("/api/values")
@@ -99,7 +105,9 @@ def values():
         tridata_dict["One-time Release"] = ONE_TIME_RELEASE
         tridata_list.append(tridata_dict)        
 
-    return jsonify(tridata_list)
+    response = jsonify(tridata_list)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 # UNIQUE LIST OF CHEMICALS
 @app.route("/api/chemicals")
@@ -117,7 +125,9 @@ def chemicals():
         chemical_dict["Chemical Name"] = CHEMICAL
         chemical_list.append(chemical_dict)        
 
-    return jsonify(chemical_list)
+    response = jsonify(chemical_list)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 # UNIQUE LIST OF INDUSTRIES
 @app.route("/api/industries")
@@ -135,7 +145,9 @@ def industries():
         industry_dict["Industry Sector"] = INDUSTRY_SECTOR
         industry_list.append(industry_dict)        
 
-    return jsonify(industry_list)
+    response = jsonify(industry_list)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 # LIST OF ALL DOCUMENT NUMBERS ON TRIBAL LANDS
 @app.route("/api/tribes")
@@ -151,11 +163,47 @@ def tribes():
         tribal_dict = {}
         tribal_dict["Document Number"] = DOCUMENT_NUMBER
         tribal_dict["Tribal Land"] = TRIBAL_LAND
-        tribal_list.append(tribal_dict)        
+        tribal_list.append(tribal_dict)       
 
-    return jsonify(tribal_list)
+    response = jsonify(tribal_list)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 
+#@app.route("/api/all", methods=["GET"])
+#def all():
+#    session = Session(engine)
+
+#    results = session.query(Tri_data.DOCUMENT_NUMBER, Tri_data.FACILITY_NAME, Tri_data.PARENT_COMPANY_NAME, Tri_data.LATITUDE, Tri_data.LONGITUDE, Tri_data.FEDERAL_FACILITY, Tri_data.CARCINOGEN, Tri_data.ON_SITE_RELEASE_TOTAL, Tri_data.INDUSTRY_SECTOR_CODE, Tri_data.REGION, Tri_data.STATE, Tri_data.SRS_ID, Tri_data.METAL_CATEGORY, Tri_data.OFF_SITE_RELEASE_TOTAL, Tri_data.OFF_SITE_RECYCLED_TOTAL, Tri_data.ONE_TIME_RELEASE, Tri_data.TRIBAL_LAND, Industry.INDUSTRY_SECTOR, Chemicals.CHEMICAL).all()
+    
+#    session.close()
+
+#    all_list = []
+#    for DOCUMENT_NUMBER, FACILITY_NAME, PARENT_COMPANY_NAME, LATITUDE, LONGITUDE, FEDERAL_FACILITY, CARCINOGEN, ON_SITE_RELEASE_TOTAL, INDUSTRY_SECTOR_CODE, REGION, STATE, TRIBAL_LAND, SRS_ID, METAL_CATEGORY, OFF_SITE_RECYCLED_TOTAL, OFF_SITE_RELEASE_TOTAL, ONE_TIME_RELEASE, INDUSTRY_SECTOR, CHEMICAL in results:
+#        all_dict = {}
+#        all_dict["Document number"] = DOCUMENT_NUMBER
+#        all_dict["Facility Name"] = FACILITY_NAME
+#        all_dict["Parent Company Name"] = PARENT_COMPANY_NAME
+#        all_dict["Latitude"] = LATITUDE
+#        all_dict["Longitude"] = LONGITUDE
+#        all_dict["Federal Facility"] = FEDERAL_FACILITY
+#        all_dict["Carcinogen"] = CARCINOGEN
+#        all_dict["On-site Release Total"] = ON_SITE_RELEASE_TOTAL
+#        all_dict["Industry Sector Code"] = INDUSTRY_SECTOR_CODE
+#        all_dict["Region"] = REGION
+#        all_dict["State"] = STATE
+#        all_dict["Tribal Land"] = TRIBAL_LAND
+#        all_dict["SRS ID"] = SRS_ID
+#        all_dict["Metal Category"] = METAL_CATEGORY
+#        all_dict["Off-site Release Total"] = OFF_SITE_RELEASE_TOTAL
+#        all_dict["Off-site Recycled Total"] = OFF_SITE_RECYCLED_TOTAL
+#        all_dict["One-time Release"] = ONE_TIME_RELEASE
+#        all_dict["Industry Sector"] = INDUSTRY_SECTOR
+#        all_dict["Chemical Name"] = CHEMICAL
+#        all_list.append(all_dict)        
+#    response = jsonify(all_list)
+#    response.headers["Access-Control-Allow-Origin"] = "*"
+#    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
